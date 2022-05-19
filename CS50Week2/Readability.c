122c3e7996a56cf739c8d9ab5774252e2acacd8c
Luca Lowndse <104835046+LucaLow@users.noreply.github.com>
2022-05-19 17:13:10 +1000

    Update Readability.c

--- CS50Week2/Readability.c
+++ CS50Week2/Readability.c
@@ -25,49 +25,66 @@ int main(void)
 
 }
 int CountSentences(string s){
-    char Buffer[3] = {'.','!','?'};
+    char Buffer[3] = {'.', '!', '?'};
     int SentenceCount = 0;
-    for(int x = 0, Length = strlen(s); x < 3; x++){
-        for(int z = 0; z <= Length; z++){
-            if(s[z] == Buffer[x]){
+    for (int x = 0, Length = strlen(s); x < 3; x++)
+    {
+        for(int z = 0; z <= Length; z++)
+        {
+            if (s[z] == Buffer[x])
+            {
                 SentenceCount++;
             }
         }
     }
 return SentenceCount;
 }
-int CountWords(string s){
+int CountWords(string s)
+{
     char Buffer = ' ';
     int WordCount = 1;
-    for(int x = 0, Length = strlen(s); x <= Length; x++){
-        if(s[x] == Buffer){
+    for (int x = 0, Length = strlen(s); x <= Length; x++)
+    {
+        if (s[x] == Buffer)
+        {
             WordCount++;
         }
     }
 return WordCount;
 }
-int CountLetters(string s){
+int CountLetters(string s)
+{
     int LetterCount = 0;
-    for(int x = 0, Length = strlen(s); x <= Length; x++){
-        if(s[x] >= 97 && s[x] <= 122){
+    for (int x = 0, Length = strlen(s); x <= Length; x++)
+    {
+        if (s[x] >= 97 && s[x] <= 122)
+        {
             LetterCount++;
-        } else if(s[x] >= 65 && s[x] <= 90){
+        }
+        else if(s[x] >= 65 && s[x] <= 90)
+        {
             LetterCount++;
         }
     }
     return LetterCount;
 }
-string CalculateYearLevel(int SentenceCount, int WordCount, int LetterCount, string s){
+string CalculateYearLevel(int SentenceCount, int WordCount, int LetterCount, string s)
+{
     int L = LetterCount * 100 / WordCount;
     int S = SentenceCount * 100 / WordCount;
     float YearLevel = 0.0588 * L - 0.296 * S - 15.8 - 0.04;
-    if (YearLevel < 1){
+    if (YearLevel < 1)
+    {
         printf("Before Grade 1\n");
         return "Before Grade 1";
-    } else if (YearLevel >= 16){
+    }
+    else if (YearLevel >= 16)
+    {
         printf("Grade 16+\n");
         return "Grade 16+";
-    } else{
+    }
+    else
+    {
         printf("%f\n", YearLevel);
         int x = round(YearLevel);
         printf("Grade %i\n", x);
